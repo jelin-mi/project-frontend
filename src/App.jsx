0025171c007d02cf31d8377ae653f04c1351dea7
“jelin.mi” <jelin.mi@gmail.com>
2022-03-04 20:42:31 +0100

    404 page added to App.js

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import IsAnon from './components/IsAnon';
 import IsPrivate from './components/IsPrivate';
 /* import Navbar from './components/Navbar'; */
 import { AuthProviderWrapper } from './context/auth.context';
+import Error404 from './pages/Error404';
 import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
 import Protected from './pages/Protected';
@@ -38,7 +39,7 @@ function App() {
             </IsAnon>
           }
         />
-        <Route path="*" element={<div>404 Not Found</div>} />
+        <Route path="*" element={<Error404 />} />
       </Routes>
     </AuthProviderWrapper>
   );
