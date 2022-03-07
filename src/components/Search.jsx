6a5c9d04158cee1f5146e76f339b1a3c6bdeb7cb
“jelin.mi” <jelin.mi@gmail.com>
2022-03-07 21:44:03 +0100

    Search added, reset does not work

---
+++ src/components/Search.jsx
@@ -0,0 +1,13 @@
+function Search({ onFilter, handleReset }) {
+  const handleSearch = e => {
+    onFilter(e.target.value);
+  };
+
+  return (
+    <div className="search">
+      <input type="text" placeholder="Search a movie..." onChange={handleSearch} />
+      <button onClick={handleReset}>Reset</button>
+    </div>
+  );
+}
+export default Search;
