e13dad6da81f3a1af3b3aade1c1fd00836ac9016
“jelin.mi” <jelin.mi@gmail.com>
2022-03-10 18:14:56 +0100

    user perfil created

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,17 +1,30 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../context/auth.context";
-
-
-
-//TODO
+import apiService from "../services/api.service";
 
 function Profile() {
+  const [ profile, setProfile] = useState({});
   const { /* isLoggedIn, */ /* user, */ logOutUser } = useContext(AuthContext);
   
+  useEffect(() => {
+    apiService
+    .getProfile({})
+    .then(response => {
+      console.log(response.data);
+      console.table(response.data);
+      setProfile(response.data)
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }, []);
+
+  console.log(profile);
 
   return (
     <>
       <h1>User profile</h1>
+      <p>{profile.email}</p>
       <button onClick={logOutUser}>Logout</button>
     </>
   );
